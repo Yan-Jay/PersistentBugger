3fa6f60f24efea1ac368f8fbe8f51947988b8599
DESKTOP-99UP9B2\YanJ <kj929409@gmail.com>
2018-09-08 10:09:10 +0800

    complete the test

---
+++ PersistentBugger/PersistentBugger/Persist.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersistentBugger
+{
+    public class Persist
+    {
+        public static int Persistence(long n)
+        {
+            if (n < 10) return 0;
+            var count = 0;
+            return Temp(n,count);
+        }
+
+        private static int Temp(long n,int count)
+        {
+            var temp = 1;
+            var i = n.ToString().ToCharArray();
+            int[] j = Array.ConvertAll(i, new Converter<char, int>(CharToInt));
+            foreach (var target in j)
+            {
+                temp *= target;
+            }
+
+            count++;
+
+            if (temp < 10)
+            {
+                return count;
+            }
+
+            return Temp(temp, count);
+        }
+
+        public static int CharToInt(char pf)
+        {
+            return pf-48;
+        }
+    }
+}
---
+++ PersistentBugger/PersistentBuggerTests/PersistTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersistentBugger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace PersistentBugger.Tests
+{
+    //[TestClass()]
+    //public class PersistTests
+    //{
+    //    [TestMethod()]
+    //    public void PersistenceTest()
+    //    {
+    //        Assert.Fail();
+    //    }
+    //}
+    [TestFixture]
+    public class PersistTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("****** Basic Tests");
+            Assert.AreEqual(3, Persist.Persistence(39));
+            Assert.AreEqual(0, Persist.Persistence(4));
+            Assert.AreEqual(2, Persist.Persistence(25));
+            Assert.AreEqual(4, Persist.Persistence(999));
+        }
+    }
+}
